aef6094d4e0cad1cce00c4ac2b624ab5e2c5d935
Brad Barrows <bebrws@gmail.com>
2020-09-13 06:02:04 -0700

    Add match option

--- src/jsin.ts
+++ src/jsin.ts
@@ -4,7 +4,7 @@ import yargs from 'yargs';
 
 const argv = yargs.options({
     json: { type: 'boolean', default: false },
-    eval: { type: 'string', default: '' }
+    match: { type: 'string', default: '' }
   }).argv;
 
   debugger
@@ -12,13 +12,18 @@ const rl: readline.Interface = readline.createInterface({input: process.stdin, o
 
 if (argv._.length != 1) {
   console.log("Usage:");
-  console.log("  jsin (optional: --json) string-to-evaluate-line-from-stdin-with");
+  console.log("  jsin (optional: --json) (optional: --match string-to-eval-to-see-if-line-matched) string-to-evaluate-line-from-stdin-with");
   console.log("  For ex:")
   console.log("  echo \"some\\nline\" | jsin \"l.replace(/e/, 'a')\"");
+  console.log("  or: ")
+  console.log("  echo '## Section 1\\n### Sub Section 1\\n### Sub Section 2\\n## Section 2\\n### Sub Section 1-2' | jsin --match \"l.match(/#/)\" \"l.replace('###', '    - ').replace('##', '- ')\"");
   process.exit(1);
 }
 
 rl.on('line', (rll: string): void => {
     var l: string | object = argv.json ? JSON.parse(rll) : rll;
-    console.log(`${eval(argv._[0])}`);
+    const out = argv.match.length ? (eval(argv.match) ? eval(argv._[0]) : "") : eval(argv._[0]);
+    if (out.length) {
+      console.log(`${out}`);
+    }
 });
