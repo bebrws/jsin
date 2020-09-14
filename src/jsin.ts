4004794e58b894cb84b999c52226c0f0473981ee
Brad Barrows <bebrws@gmail.com>
2020-09-14 00:00:10 -0700

    update notes

--- src/jsin.ts
+++ src/jsin.ts
@@ -1,7 +1,9 @@
 #!/usr/local/bin/node
 import readline from 'readline';
 import yargs from 'yargs';
+import plist from 'plist';
 import jsyaml from 'js-yaml';
+import chalk from 'chalk';
 
 // Pretty json is imported to be used from an eval statement if needed
 import pj from '@slimio/pretty-json';
@@ -21,19 +23,29 @@ const argv = yargs.options({
 const rl: readline.Interface = readline.createInterface({input: process.stdin, output: process.stdout, terminal: false});
 
 if (argv._.length != 1) {
-  console.log("Usage:");
-  console.log("  jsin --yamlout --whole --json --yaml --match positional-command-line-arg--a-string-to-evaluate-line-from-stdin-with");
-  console.log("        optional: --whole    = put all lines into one string for parsing with whichever de-serialization method is chosen"); 
-  console.log("        optional: --json     = parse input as json"); 
-  console.log("        optional: --yamlout  = print output object as yaml");   
-  console.log("        optional: --yaml     = parse input as yaml"); 
-  console.log("        optional: --match    = a string to eval to check if each string matches, should return a truthy or falsey value"); 
-  console.log("        required: The final command line argument needs to a string which will be evaled in a context where the variable l");
-  console.log("                  is either a string representing a new line, the entire read of stdin, or an object parsed from json or yaml.");
-  console.log("  For ex:");
-  console.log("  echo \"some\\nline\" | jsin \"l.replace(/e/, 'a')\"");
-  console.log("  or: ");
-  console.log("  echo '## Section 1\\n### Sub Section 1\\n### Sub Section 2\\n## Section 2\\n### Sub Section 1-2' | jsin --match \"l.match(/#/)\" \"l.replace('###', '    - ').replace('##', '- ')\"");
+  console.log(chalk.yellow(`\njsin usage and examples:\n`));
+  console.log(`A tool for parsing files line by line or as a while that are ascii/unicode and formatted as either`);
+  console.log(`json, yaml, or are an OSX/iOS plist.\n\n`);
+  console.log(`  ${chalk.cyan("jsin")} ${chalk.green("--plist")} ${chalk.green("--plistout")} ${chalk.green("--yamlout")} ${chalk.green("--whole")} ${chalk.green("--json")} ${chalk.green("--yaml")} ${chalk.green("--match")} positional-${chalk.green("unnamed-command-line-arg")}-a-string-to-evaluate-line-from-stdin-with`);
+  console.log(`        optional: ${chalk.green("--whole")}     = put all lines into one string for parsing with whichever de-serialization method is chosen. String is variable l`); 
+  console.log(`        optional: ${chalk.green("--json")}      = parse input as json into an object, l`); 
+  console.log(`        optional: ${chalk.green("--yamlout")}   = print output object as yaml`);   
+  console.log(`        optional: ${chalk.green("--yaml")}      = parse input as yaml into an object, l`); 
+  console.log(`        optional: ${chalk.green("--plistout")}  = print output object and generate plist xml output`);   
+  console.log(`        optional: ${chalk.green("--plist")}     = parse input plist xml into an object, l`);   
+  console.log(`        optional: ${chalk.green("--match")}     = a string to eval to check if each string matches, should return a truthy or falsey value`); 
+  console.log(`        required: The ${chalk.green("final command line argument")} needs to a string which will be evaled in a context where the variable l`);
+  console.log(`                  is either a string representing a new line, the entire read of stdin, or an object parsed from json or yaml.`);
+  console.log(`\n  For ex:\n`);
+  console.log(chalk.blue(`  echo \"some\\nline\" | jsin \"l.replace(/e/, 'a')\"`));
+  console.log(`\n  or:\n`);
+  console.log(chalk.blue(`  echo '## Section 1\\n### Sub Section 1\\n### Sub Section 2\\n## Section 2\\n### Sub Section 1-2' | jsin --match \"l.match(/#/)\" \"l.replace('###', '    - ').replace('##', '- ')\"`));
+  console.log(`\n  or a helpful bash function for setting a terminals opacity:\n`);
+  console.log(chalk.blue(`  function alaOpacity() {
+    cat ~/.config/alacritty/alacritty.yml | jsin --yaml --yamlout --whole "function r(l, o) { l.background_opacity=Number(o); return l; }  r(l, \"$1\"); " >  ~/.config/alacritty/alacritty.new
+    mv ~/.config/alacritty/alacritty.new ~/.config/alacritty/alacritty.yml
+  }`))
+  console.log(`\n\n  Check the README.md file for other examples. https://github.com/bebrws/jsin/blob/master/README.md\n\n`);
   process.exit(1);
 }
 
